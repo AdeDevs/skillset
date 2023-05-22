bc423bfbe732c02bf88080c5b4a75a4f4fd1171a
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-05-22 09:35:47 +0100

    updated

---
+++ src/screens/HomeScree.jsx
@@ -0,0 +1,12 @@
+import '../styles/home.css'
+
+function HomeScreen() {
+    return (
+        <div className="container">
+            <h1>Coalitions technologies</h1>
+            <p>again?</p>
+        </div>
+    )
+}
+
+export default HomeScreen;
