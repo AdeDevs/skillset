6b91320ea6c5554c1ae28fd4444074afaf692c8e
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-05-22 13:44:25 +0100

    Updated

---
+++ src/deploy.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTgJLEqG5wywRcOzsgvg4Xe-F0eyf96r8",
+  authDomain: "coalitionskills.firebaseapp.com",
+  projectId: "coalitionskills",
+  storageBucket: "coalitionskills.appspot.com",
+  messagingSenderId: "562675490384",
+  appId: "1:562675490384:web:738df1c55951306fa03b55"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
